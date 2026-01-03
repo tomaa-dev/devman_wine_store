c7d12ad60acb32a23efc057f5280b1d283e8fdd1
tomaa-dev <lajpanovatoma@gmail.com>
2026-01-03 14:26:34 +0300

    Wrap the code in if __name__ == '__main__' / def main

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ import pandas
 from collections import defaultdict
 
 
-now = datetime.datetime.now()
-event1 = now.year
-event2 = 1920
-delta = event1-event2
-
-
 def get_year_word_form(delta):
     plural_form = 'лет'
     single_form = 'год'
@@ -26,35 +20,43 @@ def get_year_word_form(delta):
         return plural_form
 
 
-excel_data_df = pandas.read_excel(
-    'wine3.xlsx', 
-    sheet_name='Вино', 
-    usecols=['Категория', 'Название','Сорт', 'Цена', 'Картинка', 'Акция'],
-    na_values=['N/A', 'NA'],
-    keep_default_na=False
-)
+def main():
+    now = datetime.datetime.now()
+    event1 = now.year
+    event2 = 1920
+    delta = event1-event2
 
-wines = excel_data_df.to_dict(orient='records')
-wine_categories = defaultdict(list)
-for wine in wines:
-    wine_categories[wine['Категория']].append(wine)
+    excel_data_df = pandas.read_excel(
+        'wine3.xlsx', 
+        sheet_name='Вино', 
+        usecols=['Категория', 'Название','Сорт', 'Цена', 'Картинка', 'Акция'],
+        na_values=['N/A', 'NA'],
+        keep_default_na=False
+    )
+
+    wines = excel_data_df.to_dict(orient='records')
+    wine_categories = defaultdict(list)
+    for wine in wines:
+        wine_categories[wine['Категория']].append(wine)
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(
+        wine_categories=wine_categories,
+        age_of_the_winery=delta,
+        year=get_year_word_form(delta)
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
-
-rendered_page = template.render(
-    wine_categories=wine_categories,
-    age_of_the_winery=delta,
-    year=get_year_word_form(delta)
-)
-
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+if __name__ == '__main__':
+    main()
